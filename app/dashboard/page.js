c73ac67a44e554cdaabcd8b1faf3eb85458c5f55
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2025-05-08 13:53:21 +0700

    refactor: remove dashboard main content and add preline dependency
    - Removed the main content section from the Dashboard component in page.js.
    - Added preline as a dependency in package.json and package-lock.json.

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -4,10 +4,6 @@ export default function Dashboard() {
   return (
     <>
       <Header />
-      <main className="bg-gray-100 min-h-screen p-4">
-        <h1 className="text-2xl font-bold text-center">Dashboard</h1>
-        {/* Konten dashboard lainnya */}
-      </main>
     </>
   );
 }
