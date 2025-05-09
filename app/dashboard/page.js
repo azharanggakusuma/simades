3d41a22151d37ce1e7446979f7b135473578891c
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2025-05-09 17:42:07 +0700

    feat: update Dashboard layout to improve Stats component positioning

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -12,11 +12,15 @@ export default function Dashboard() {
 
       <main className="flex-grow p-4 sm:ml-64">
         <Breadcrumb />
-        
+
+        <div className="mt-8">
+          <Stats />
+        </div>
+        {/*
         <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-8">
           <Stats />
         </div>
-        
+        */}
       </main>
 
       <Footer />
