56518832a376e0b49842b2efd4ef11761185508b
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2025-05-09 13:26:52 +0700

    feat: add Navbar component and integrate it into the Dashboard page; update Footer layout for improved positioning

--- app/dashboard/components/Footer.tsx
+++ app/dashboard/components/Footer.tsx
@@ -1,9 +1,11 @@
 export default function Footer() {
   return (
     <div className="sm:ml-64">
-      <footer className="mt-auto w-full bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-700 py-3 px-6 flex justify-between items-center text-sm text-gray-500 dark:text-gray-400">
+      <footer className="fixed bottom-0 mt-auto w-full bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-700 py-3 px-6 flex justify-between items-center text-sm text-gray-500 dark:text-gray-400">
         <span>© {new Date().getFullYear()} Diskominfo Kab. Cirebon</span>
-        <span className="text-xs">Version 1.0.0</span>
+        <div className="sm:mr-64">
+          <span className="text-xs">Version 1.0.0</span>
+        </div>
       </footer>
     </div>
   );
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Stats from "./components/Stats";
 import Footer from "./components/Footer";
@@ -5,6 +6,7 @@ import Footer from "./components/Footer";
 export default function Dashboard() {
   return (
     <div className="bg-gray-100">
+      <Navbar />
       <Sidebar />
       <div className="p-4 sm:ml-64">
         <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
