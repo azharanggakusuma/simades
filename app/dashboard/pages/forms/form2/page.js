3266024a35699c2227b66fcdfbb75182c34c7a66
Azharangga Kusuma <azharanggakusuma01@gmail.com>
2025-05-12 18:10:10 +0700

    feat: add user pages with layout components including Navbar, Sidebar, Footer, and Breadcrumb

---
+++ app/dashboard/pages/forms/form2/page.js
@@ -0,0 +1,25 @@
+import Navbar from "../../../components/Navbar";
+import Sidebar from "../../../components/Sidebar";
+import Footer from "../../../components/Footer";
+import Breadcrumb from "../../../components/Breadcrumb";
+
+export default function Users() {
+  return (
+    <div className="bg-gray-50 min-h-screen flex flex-col">
+      <Navbar />
+      <Sidebar />
+
+      <main className="flex-grow sm:ml-64">
+        <div className="p-6">
+          <Breadcrumb />
+        </div>
+
+        <div className="mb-8 mt-[-45px]">
+        
+        </div>
+      </main>
+      
+      <Footer />
+    </div>
+  );
+}
